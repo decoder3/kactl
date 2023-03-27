1346d6c39029909eeb26dadeb6a1622313402cb6
decoder3 <pcshubham889@gmail.com>
2023-03-27 22:50:34 +0530

    v1

--- content/contest/template.cpp
+++ content/contest/template.cpp
@@ -9,6 +9,7 @@ typedef pair<int, int> pii;
 typedef vector<int> vi;
 
 int main() {
-	cin.tie(0)->sync_with_stdio(0);
-	cin.exceptions(cin.failbit);
+  cin.tie(0)->sync_with_stdio(0);
+  cin.exceptions(cin.failbit);
 }
+
