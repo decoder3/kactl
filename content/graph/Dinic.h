ec93a369276ecd931a5f7b764ea2d4fdaf3eb800
Anshuman Panda <anshumanpanda211@gmail.com>
2023-04-01 10:02:10 +0530

    upd cut set

--- content/graph/Dinic.h
+++ content/graph/Dinic.h
@@ -36,17 +36,22 @@ struct Dinic {
 	}
 	ll calc(int s, int t) {
 		ll flow = 0; q[0] = s;
-		rep(L,0,31) do { // 'int L=30' maybe faster for random data
-			lvl = ptr = vi(sz(q));
-			int qi = 0, qe = lvl[s] = 1;
-			while (qi < qe && !lvl[t]) {
-				int v = q[qi++];
-				for (Edge e : adj[v])
-					if (!lvl[e.to] && e.c >> (30 - L))
-						q[qe++] = e.to, lvl[e.to] = lvl[v] + 1;
-			}
-			while (ll p = dfs(s, t, LLONG_MAX)) flow += p;
-		} while (lvl[t]);
+		rep(L,0,31) 
+		{
+			do { // 'int L=30' maybe faster for random data
+				lvl = ptr = vi(sz(q));
+				int qi = 0, qe = lvl[s] = 1;
+				while (qi < qe && !lvl[t]) {
+					int v = q[qi++];
+					for (Edge e : adj[v])
+						if (!lvl[e.to] && e.c >> (30 - L))
+							q[qe++] = e.to, lvl[e.to] = lvl[v] + 1;
+				}
+				while (ll p = dfs(s, t, LLONG_MAX)) flow += p;
+			} while (lvl[t]);
+		}
+		vi cut;
+		rep(i,0,n) if(lvl[i]) cut.push_back(i);
 		return flow;
 	}
 	bool leftOfMinCut(int a) { return lvl[a] != 0; }
